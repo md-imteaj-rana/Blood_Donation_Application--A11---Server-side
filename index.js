acb3044bdadd98f7861e0d61364f878de1222a4a
MD. Imteaj Rana <imteaj.rana2@gmail.com>
2025-12-16 01:16:01 +0600

    3rd

--- index.js
+++ index.js
@@ -11,9 +11,15 @@ app.use(express.json())
 
 // till here
 
-// new started
-
 // checking the connection to the homepage
 app.get('/', (req, res) => {
     res.send("Blood Donation Application - A11 : Backend connected.")
 })
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}.`)
+})
+
+// we can keep same till here
+
+// new started
